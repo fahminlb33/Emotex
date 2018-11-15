135301f508f43c43ef23961ea5838537646ff0db
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2018-11-15 17:47:37 +0700

    refactored to single file

--- Emotex.MachineLearning/Helpers.cs
+++ Emotex.MachineLearning/Helpers.cs
@@ -5,8 +5,8 @@ namespace Emotex.MachineLearning
 {
     internal class Helpers
     {
-        public static string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static readonly string WorkingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-        public static string DatasetPath = Path.Combine(WorkingDirectory, "TrainingData");
+        public static readonly string DatasetPath = Path.Combine(WorkingDirectory, "TrainingData.xls");
     }
 }
